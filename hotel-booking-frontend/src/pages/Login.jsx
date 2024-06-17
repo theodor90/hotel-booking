4cceca78ccc528a2d8a5339826580bbcb0ebbc58
david <david@DESKTOP-68N5QD0>
2024-06-17 12:25:48 +0200

    First commit for login and register form with pages

--- hotel-booking-frontend/src/pages/Login.jsx
+++ hotel-booking-frontend/src/pages/Login.jsx
@@ -1,40 +1,12 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import LoginForm from '../components/Form/LoginForm';
 import api from '../services/api';
 
 const Login = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState('');
-    const navigate = useNavigate();
-
-    const handleLogin = async (e) => {
-        e.preventDefault();
-        try {
-            const response = await api.login(email, password);
-            localStorage.setItem('token', response.data);
-            navigate('/dashboard');
-        } catch (err) {
-            console.error('Error details:', err.response.data);
-            setError('Login failed. Please try again.');
-        }
-    };
-
     return (
         <div>
-            <h2>Login</h2>
-            <form onSubmit={handleLogin}>
-                <div>
-                    <label>Email</label>
-                    <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
-                </div>
-                <div>
-                    <label>Password</label>
-                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
-                </div>
-                <button type="submit">Login</button>
-            </form>
-            {error && <p>{error}</p>}
+         <LoginForm />
         </div>
     );
 };
