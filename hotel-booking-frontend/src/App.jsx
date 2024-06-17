7d86457bc69b1190ef1bc585831fe2ca1fd4f0b4
al-sw <alanjunior.sweeney@gmail.com>
2024-06-17 10:25:40 +0200

    3

--- hotel-booking-frontend/src/App.jsx
+++ hotel-booking-frontend/src/App.jsx
@@ -1,47 +1,11 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Navigate,
-} from "react-router-dom";
-import PropTypes from "prop-types";
-import Register from "./pages/Register";
-import Login from "./pages/Login";
-import Dashboard from "./pages/dashboard/Dashboard";
-import Footer from "./components/Footer/Footer";
-import Buttons from "./components/Buttons/Buttons";
-
-const PrivateRoute = ({ children }) => {
-  const token = localStorage.getItem("token");
-  return token ? children : <Navigate to="/login" />;
-};
-
-PrivateRoute.propTypes = {
-  children: PropTypes.node.isRequired,
-};
+import Hero from "./components/hero/Hero";
 
 const App = () => {
   return (
     <>
-      <Buttons />
-      <Footer />
+      <Hero />
     </>
-    // <Router>
-    //   <Routes>
-    //     <Route path="/register" element={<Register />} />
-    //     <Route path="/login" element={<Login />} />
-    //     <Route
-    //       path="/dashboard"
-    //       element={
-    //         <PrivateRoute>
-    //           <Dashboard />
-    //         </PrivateRoute>
-    //       }
-    //     />
-    //   </Routes>
-    //   <Footer />
-    // </Router>
   );
 };
 
---
+++ hotel-booking-frontend/src/components/hero/Hero.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Hero.css";
+import "../buttons/Buttons";
+
+export default function Hero() {
+  return (
+    <div>
+      <div className="hero-bg">
+        <div className="hero-content">
+          <div>
+            <h1>Book your rooms now</h1>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid
+              error repudiandae tenetur accusantium.
+            </p>
+          </div>
+          <div>
+            <button className="btn btn-red">Hotel</button>
+            <button className="btn btn-red-reverse">Hotel</button>
+          </div>
+        </div>
+      </div>
+      <div className="search-bar"></div>
+    </div>
+  );
+}
