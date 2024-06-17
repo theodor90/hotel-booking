fbb4fdce6112408ac48ec4ca3a2e1294942744df
al-swe <71519437+al-swe@users.noreply.github.com>
2024-06-17 15:07:22 +0200

    Update Featured.jsx

--- hotel-booking-frontend/src/components/featured/Featured.jsx
+++ hotel-booking-frontend/src/components/featured/Featured.jsx
@@ -1,1 +1,40 @@
+import React, { useState, useEffect } from "react";
+import data from "./HotelData.json";
+import "./Featured.css";
+import "../Buttons/Buttons";
 
+export default function Featured() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % data.length);
+    }, 5000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <>
+      <h1>Featured Hotels</h1>
+      <div className="carousel">
+        {data.map((item, index) => (
+          <div
+            key={index}
+            className={`carousel-item ${
+              index === currentIndex ? "active" : ""
+            }`}
+          >
+            <img src={item.image} alt={item.hotelName} />
+            <div className="carousel-caption">
+              <h2>{item.hotelName}</h2>
+              <h5>{item.Location}</h5>
+              <p>{item.description}</p>
+              <button className="btn btn-red">More info</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
