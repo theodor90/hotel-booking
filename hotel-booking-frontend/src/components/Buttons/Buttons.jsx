0d593e1f038b8d2f8fc900f25ca5f486ea2a64d8
al-sw <alanjunior.sweeney@gmail.com>
2024-06-17 09:01:20 +0200

    3

--- hotel-booking-frontend/src/components/Buttons/Buttons.jsx
+++ hotel-booking-frontend/src/components/Buttons/Buttons.jsx
@@ -6,10 +6,13 @@ export default function Buttons() {
     <div className="container">
       <button className="btn btn-red">Button</button>
       <button className="btn btn-red-reverse">Button</button>
+
       <button className="btn btn-green">Button</button>
       <button className="btn btn-green-reverse">Button</button>
+
       <button className="btn btn-blue">Button</button>
       <button className="btn btn-blue-reverse">Button</button>
+
       <button className="btn btn-dark-blue">Button</button>
       <button className="btn btn-dark-blue-reverse">Button</button>
     </div>
