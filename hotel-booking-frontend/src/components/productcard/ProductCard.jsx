59fb680481d135aee90680c96a6b8da610f87a89
MinaStormcloud <164744381+MinaStormcloud@users.noreply.github.com>
2024-06-17 15:48:06 +0200

    Button

--- hotel-booking-frontend/src/components/productcard/ProductCard.jsx
+++ hotel-booking-frontend/src/components/productcard/ProductCard.jsx
@@ -11,7 +11,7 @@ return(
           <h4 className="card-title shop-item-title">  Cassandra Hotel </h4>
           <p>Sunshine </p>
           <p className="card-text shop-item-price"> </p>
-          <button type="button" className="btn btn-warning mt-auto shop-item-button"><i className="fa-solid fa-cart-shopping "></i>Book Now</button>                    
+          <button className="btn-dark-blue">Book Now</button>                    
         </div>
       </div>
   </div>
