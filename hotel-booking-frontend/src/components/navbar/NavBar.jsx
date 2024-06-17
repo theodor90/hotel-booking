323b8a6f3be8d6a2764ffe8898d7b554b77296a2
al-sw <alanjunior.sweeney@gmail.com>
2024-06-17 11:39:18 +0200

    NavBar updated.

--- hotel-booking-frontend/src/components/navbar/NavBar.jsx
+++ hotel-booking-frontend/src/components/navbar/NavBar.jsx
@@ -1,6 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./NavBar.css";
+import "../../css/Buttons.css";
 
-export default function NavBar() {
-  return <div>NavBar</div>;
-}
+const NavBar = () => {
+  return (
+    <nav>
+      <h1>LOGO</h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/register">Register</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/dashboard">Dashboard</Link>
+        </li>
+      </ul>
+      <button className="btn btn-blue">Login</button>
+    </nav>
+  );
+};
+export default NavBar;
