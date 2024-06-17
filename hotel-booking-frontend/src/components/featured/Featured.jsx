164829e6e7c7acd8cac8f924d785e45599742246
al-sw <alanjunior.sweeney@gmail.com>
2024-06-17 15:18:15 +0200

    1

--- hotel-booking-frontend/src/components/featured/Featured.jsx
+++ hotel-booking-frontend/src/components/featured/Featured.jsx
@@ -16,22 +16,24 @@ export default function Featured() {
 
   return (
     <>
-      <div className="carousel">
-        {data.map((item, index) => (
-          <div
-            key={index}
-            className={`carousel-item ${
-              index === currentIndex ? "active" : ""
-            }`}
-          >
-            <img src={item.image} alt={item.hotelName} />
-            <div className="carousel-caption">
-              <h2>{item.hotelName}</h2>
-              <h5>{item.Location}</h5>
-              <p>{item.description}</p>
+      <div className="white-space">
+        <div className="carousel">
+          {data.map((item, index) => (
+            <div
+              key={index}
+              className={`carousel-item ${
+                index === currentIndex ? "active" : ""
+              }`}
+            >
+              <img src={item.image} alt={item.hotelName} />
+              <div className="carousel-caption">
+                <h2>{item.hotelName}</h2>
+                <h5>{item.Location}</h5>
+                <p>{item.description}</p>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </>
   );
