316b5f2773f86a204ab544fda9854747edc4760c
mattiasvlex <mattias.vinje@gmail.com>
2024-06-17 16:32:06 +0200

    Lower case folder name for searchbar

--- hotel-booking-frontend/src/components/SearchBar/SearchBar.jsx
+++ hotel-booking-frontend/src/components/SearchBar/SearchBar.jsx
@@ -1,8 +1,8 @@
 import { useState, useEffect } from "react";
 import "./SearchBar.css";
 import "../../css/Buttons.css";
-import DatePicker from "./DatePicker/DatePicker";
-import DestinationPicker from "./DestinationPicker/DestinationPicker";
+import DatePicker from "./datepicker/DatePicker";
+import DestinationPicker from "./destinationpicker/DestinationPicker";
 
 function SearchBar() {
   const [nights, setNights] = useState(0);
