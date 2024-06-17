cc7f8cf887a96f4b387bf90a071a0271c8f090d1
MinaStormcloud <164744381+MinaStormcloud@users.noreply.github.com>
2024-06-17 14:16:01 +0200

    AddedCards

--- hotel-booking-frontend/src/components/productcard/ProductCard.jsx
+++ hotel-booking-frontend/src/components/productcard/ProductCard.jsx
@@ -1,41 +1,17 @@
 import React from 'react';
+import "./ProductCard.css";
 
 const ProductCard = () => { 
 return(
-<div className="row">
-    <div className="column">
-      <div className="card">
-        <img src="" className="card-img-top shop-item-image" alt=""/>                    
-        <h4 className="card-title shop-item-title">  </h4>
-        <p className="card-text shop-item-price"> </p>
-        <button type="button" className="btn btn-warning mt-auto shop-item-button"><i className="fa-solid fa-cart-shopping "></i>Add To Cart</button>                    
-      </div>
-    </div>
-    
-    <div className="column">
-        <div className="card">
-          <img src="" className="card-img-top shop-item-image" alt="" />                    
-          <h4 className="card-title shop-item-title"> </h4>
-          <p className="card-text shop-item-price"> </p>
-          <button type="button" className="btn btn-warning mt-auto shop-item-button"><i className="fa-solid fa-cart-shopping "></i>Add To Cart</button>                    
-        </div>
-      </div>
+<div className="row">    
 
       <div className="column">
         <div className="card">
-          <img src="" className="card-img-top shop-item-image" alt="..."/>                    
-          <h4 className="card-title shop-item-title">  </h4>
+          <img src="./src/assets/images/danai-tsoutreli-unsplash.jpg" className="card-img-top shop-item-image" alt="..."/>                    
+          <h4 className="card-title shop-item-title">  Cassandra Hotel </h4>
+          <p>Sunshine </p>
           <p className="card-text shop-item-price"> </p>
-          <button type="button" className="btn btn-warning mt-auto shop-item-button"><i className="fa-solid fa-cart-shopping "></i>Add To Cart</button>                    
-        </div>
-      </div>
-
-      <div className="column">
-        <div className="card">
-          <img src="" className="card-img-top shop-item-image" alt="..."/>                    
-          <h4 className="card-title shop-item-title">  </h4>
-          <p className="card-text shop-item-price"> </p>
-          <button type="button" className="btn btn-warning mt-auto shop-item-button"><i className="fa-solid fa-cart-shopping "></i>Add To Cart</button>                    
+          <button type="button" className="btn btn-warning mt-auto shop-item-button"><i className="fa-solid fa-cart-shopping "></i>Book Now</button>                    
         </div>
       </div>
   </div>
--- hotel-booking-frontend/src/pages/about/About.js
+++ hotel-booking-frontend/src/pages/about/About.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Home = () => {
+const About = () => {
   return <h1>About</h1>;
 };
 
