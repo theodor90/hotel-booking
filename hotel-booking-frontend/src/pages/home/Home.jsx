82b33a424a918a7a1d0b8fc10fdd4cd4d752939a
al-swe <71519437+al-swe@users.noreply.github.com>
2024-06-18 13:38:49 +0200

    Update Home.jsx

--- hotel-booking-frontend/src/pages/home/Home.jsx
+++ hotel-booking-frontend/src/pages/home/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./Home.css";
 import Hero from "../../components/hero/Hero";
 import Featured from "../../components/featured/Featured";
+import BackToTop from "../../components/backtotop/BackToTop";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Featured />
+      <BackToTop />
     </>
   );
 }
