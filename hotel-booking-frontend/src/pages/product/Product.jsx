9032eee1d4f27727532fe368def80c525effda50
MinaStormcloud <164744381+MinaStormcloud@users.noreply.github.com>
2024-06-19 12:47:28 +0200

    Update Product.jsx

--- hotel-booking-frontend/src/pages/product/Product.jsx
+++ hotel-booking-frontend/src/pages/product/Product.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import Grid from '../../components/grid/Grid';
 import ProductCard from '../../components/productcard/ProductCard';
 
 export default function Product() {
   return (
     <div>      
-      <h1>Product Page</h1>
-      <ProductCard/>
+      <h1>Hotels</h1>           
+      <Grid/>
     </div>
   );
 }
