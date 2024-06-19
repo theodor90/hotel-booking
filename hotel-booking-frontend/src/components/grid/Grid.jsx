4241ccd458eddc87872f2ac6221bef959956c1e5
MinaStormcloud <164744381+MinaStormcloud@users.noreply.github.com>
2024-06-19 12:45:07 +0200

    Update Grid.jsx

--- hotel-booking-frontend/src/components/grid/Grid.jsx
+++ hotel-booking-frontend/src/components/grid/Grid.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
-//import './Grid.css';
+import './Grid.css';
+import data from './../productcard/hotels.json';
 
 function Grid() {
 
-  return <div class="grid grid-cols-4 gap-10">
-    {
-        
-      
+  return <div className="grid grid-cols-4 gap-10">
+    {data.map(hotel => (
+      <div key={hotel.image} className="card">
+        <div key={hotel.name}>
+            <img src={hotel.image} alt={`${hotel.name}`} />
+            <h2>{hotel.name}</h2>
+            <h4>{hotel.location}</h4> 
+            <p>{hotel.description}</p> 
+            <button>Book Now</button>                                       
+        </div>        
+      </div >
+    ))
     }
   </div >
 }
