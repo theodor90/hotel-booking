5efc0a769a53ae18482fecb288bb66ef5f877380
david <david@DESKTOP-68N5QD0>
2024-06-26 11:32:38 +0200

    Update Grid with Rooms

--- hotel-booking-frontend/src/components/grid/Grid.jsx
+++ hotel-booking-frontend/src/components/grid/Grid.jsx
@@ -1,33 +1,57 @@
 import { useEffect, useState } from "react";
 import "./Grid.css";
 
-function Grid() {
-  const [hotels, setHotels] = useState([]);
+function Grid({ type }) {
+  const [data, setData] = useState([]);
 
   useEffect(() => {
-    fetch("https://localhost:7204/api/Hotels")
+    const url =
+      type === "hotels"
+        ? "https://localhost:7204/api/Hotels"
+        : "https://localhost:7204/api/Rooms";
+
+    fetch(url)
       .then((response) => response.json())
-      .then((data) => setHotels(data))
-      .catch((error) => console.error("Error fetching hotels:", error));
-  }, []);
+      .then((data) => setData(data))
+      .catch((error) => console.error(`Error fetching ${type}:`, error));
+  }, [type]);
+
   return (
-    // RENAME ALL CLASSES
-    <div className="grid">
-      {hotels.map((hotel) => (
-        <div key={hotel.hotelId} className="card">
-          <div>
-            <img
-              src={hotel.imgUrl}
-              alt={`${hotel.name}`}
-              className="card-image"
-            />
-            <h2>{hotel.name}</h2>
-            <h4>{hotel.location}</h4>
-            <p className="hotel-description">{hotel.description}</p>
+    <div className="grid-container">
+      {type === "hotels" &&
+        data.map((hotel) => (
+          <div key={hotel.hotelId} className="card-wrapper">
+            <div>
+              <img
+                src={hotel.imgUrl}
+                alt={`${hotel.name}`}
+                className="card-image-wrapper"
+              />
+              <h2>{hotel.name}</h2>
+              <h4>{hotel.location}</h4>
+              <p className="hotel-description-text">{hotel.description}</p>
+            </div>
+            <button className="custom-btn btn-blue">Book Now</button>
           </div>
-          <button className="btn btn-blue">Book Now</button>
-        </div>
-      ))}
+        ))}
+      {type === "rooms" &&
+        data.map((room) => (
+          <div key={room.roomId} className="card-wrapper">
+            <div>
+              <img
+                src={room.imgUrl}
+                alt={`${room.name}`}
+                className="card-image-wrapper"
+              />
+              {/* <h2>{room.name}</h2> */}
+              <h2>{room.type}</h2>
+              <h4>{room.price}</h4>
+              <h4>{room.availability}</h4>
+              {/* <p className="hotel-description-text">{room.description}</p> */}
+            </div>
+            <button className="custom-btn btn-blue">Book Now</button>
+          </div>
+        ))}
     </div>
   );
 }
--- hotel-booking-frontend/src/pages/home/Home.jsx
+++ hotel-booking-frontend/src/pages/home/Home.jsx
@@ -3,12 +3,16 @@ import "./Home.css";
 import Hero from "../../components/hero/Hero";
 import Featured from "../../components/featured/Featured";
 import BackToTop from "../../components/backtotop/BackToTop";
+import Grid from "../../components/grid/Grid";
+
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Featured />
+      <Grid type="hotels" />
+      <Grid type="rooms" />
       <BackToTop />
     </>
   );
