64c3812ef8a9e061a0099ff172fdec54e55a010d
MinaStormcloud <164744381+MinaStormcloud@users.noreply.github.com>
2024-07-01 12:40:34 +0200

    ShoppingCartUpdates

---
+++ hotel-booking-frontend/src/components/progressbar/ProgressBar.jsx
@@ -0,0 +1,42 @@
+import "./ProgressBar.css";
+
+const ProgressBar = (props) => {
+  let selectionClass, detailsClass, checkoutClass;
+  if (props.active == 1) {
+    selectionClass = "active";
+    detailsClass = "future";
+    checkoutClass = "future";
+  } else if (props.active == 2) {
+    selectionClass = "done";
+    detailsClass = "active";
+    checkoutClass = "future";
+  } else {
+    selectionClass = "done"; //Add additional check for different from 1-3
+    detailsClass = "done";
+    checkoutClass = "active";
+  }
+
+  return (
+    <div className="progress-bar">
+      <ul className="bar">
+        <li className={selectionClass}>
+          <div className="item">
+            <b>Your selection</b>
+          </div>
+        </li>
+        <li className={detailsClass}>
+          <div className="item">
+            <b>Your details</b>
+          </div>
+        </li>
+        <li className={checkoutClass}>
+          <div className="item">
+            <b>Checkout</b>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default ProgressBar;
--- hotel-booking-frontend/src/components/userdetails/UserDetails.jsx
+++ hotel-booking-frontend/src/components/userdetails/UserDetails.jsx
@@ -2,79 +2,131 @@ import "./UserDetails.css";
 
 function UserDetails(props) {
   return (
-    <div className="user-details">
-      <h3 className="user-title">Enter your details</h3>
-      <div className="user-name">
-        <label htmlFor="firstName">
-          <b>First name</b>
-        </label>
-        <input
-          type="text"
-          placeholder=""
-          name="firstname"
-          required
-          value={props.firstName}
-          onChange={props.setFirstName}
-        />
-        <label htmlFor="lastName">
-          <b>Last name</b>
-        </label>
-        <input
-          type="text"
-          placeholder=""
-          name="lastname"
-          required
-          value={props.lastName}
-          onChange={props.setLastName}
-        />
+    <div className="user-wrapper">
+      <h3>Enter your details</h3>
+      <div className="name-container">
+        <div className="input-container">
+          <label htmlFor="firstname">
+            <b>First name</b>
+          </label>
+          <input
+            type="text"
+            placeholder=""
+            id="firstname"
+            name="firstname"
+            required
+            value={props.firstName}
+            onChange={props.setFirstName}
+          />
+        </div>
+        <div className="input-container">
+          <label htmlFor="lastname">
+            <b>Last name</b>
+          </label>
+          <input
+            type="text"
+            placeholder=""
+            id="lastname"
+            name="lastname"
+            required
+            value={props.lastName}
+            onChange={props.setLastName}
+          />
+        </div>
       </div>
-      <div className="user-container">
+      <div className="input-container">
         <label htmlFor="email">
           <b>Email address</b>
         </label>
         <input
           type="text"
           placeholder="Enter email"
+          id="email"
           name="email"
           required
           value={props.email}
           onChange={props.setEmail}
         />
       </div>
-      <div className="user-container">
+      <div className="input-container">
+        <label htmlFor="address">
+          <b>Address</b>
+        </label>
+        <input
+          type="text"
+          placeholder=""
+          id="address"
+          name="address"
+          required
+          value={props.address}
+          onChange={props.setAddress}
+        />
+      </div>
+      <div className="input-container">
+        <label htmlFor="city">
+          <b>City</b>
+        </label>
+        <input
+          type="text"
+          placeholder=""
+          id="city"
+          name="city"
+          required
+          value={props.city}
+          onChange={props.setCity}
+        />
+      </div>
+      <div className="input-container">
+        <label htmlFor="postal">
+          <b>Zip/Postal code</b>
+        </label>
+        <input
+          type="text"
+          placeholder=""
+          id="postal"
+          name="postal"
+          required
+          value={props.postal}
+          onChange={props.setPostal}
+        />
+      </div>
+      <div className="input-container">
         <label htmlFor="country">
           <b>Country/region</b>
         </label>
         <input
           type="text"
           placeholder=""
+          id="country"
           name="country"
           required
           value={props.country}
           onChange={props.setCountry}
         />
       </div>
-      <div className="user-container">
+      <div className="input-container">
         <label htmlFor="phone">
           <b>Telephone</b>
         </label>
         <input
           type="text"
           placeholder=""
+          id="phone"
           name="phone"
           required
           value={props.phone}
           onChange={props.setPhone}
         />
       </div>
-      <div className="confirmation">
-        <label htmlFor="confirmation">Yes, send me a confirmation email</label>
+      <div className="checkbox">
         <input
           type="checkbox"
+          id="confirmation"
           name="confirmation"
           defaultChecked={props.confirm}
           onClick={props.setConfirm}
         />
+        <label htmlFor="confirmation">Yes, send me a confirmation email</label>
       </div>
     </div>
   );
