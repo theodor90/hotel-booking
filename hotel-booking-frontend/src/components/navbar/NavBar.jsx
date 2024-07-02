12c3f6492c599dde8e2be5df87edd2968f3dbfac
mattiasvlex <mattias.vinje@gmail.com>
2024-07-02 13:58:51 +0200

    NavBar updated to accommodate login

--- hotel-booking-frontend/src/components/navbar/NavBar.jsx
+++ hotel-booking-frontend/src/components/navbar/NavBar.jsx
@@ -1,9 +1,18 @@
-import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
 import "./NavBar.css";
 import "../../css/Buttons.css";
 
 const NavBar = () => {
+  const token = localStorage.getItem("token");
+  const navigate = useNavigate();
+
+  const handleLogout = (event) => {
+    event.preventDefault(); // Prevent default link behavior
+    localStorage.removeItem("token");
+    navigate("/login");
+  };
+
   return (
     <nav>
       <h1>LOGO</h1>
@@ -14,23 +23,25 @@ const NavBar = () => {
         <li>
           <Link to="/hotels">Hotels</Link>
         </li>
-        {/* <li>
-          <Link to="/">Rooms</Link>
-        </li> */}
         <li>
           <Link to="/about">About</Link>
         </li>
-        {/* <li>
-          <Link to="/register">Register</Link>
-        </li> */}
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-        {/* <li>
-          <Link to="/dashboard">Dashboard</Link>
-        </li> */}
+        {!token && (
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        )}
+        {token && (
+          <>
+            <li>
+              <Link to="/dashboard">Dashboard</Link>
+            </li>
+            <li>
+              <Link to="/logout" onClick={handleLogout}>Logout</Link>
+            </li>
+          </>
+        )}
       </ul>
-      {/* <button className="btn btn-blue">Login</button> */}
     </nav>
   );
 };
