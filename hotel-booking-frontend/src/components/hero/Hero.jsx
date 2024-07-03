2b0396a23a6770feb525f866275884a83bd9f7c8
David Ruland <davidruland@MacBook-Air-som-tillhor-David.local>
2024-07-03 13:40:21 +0200

    Update hero css and hero layout

--- hotel-booking-frontend/src/components/hero/Hero.jsx
+++ hotel-booking-frontend/src/components/hero/Hero.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 import "./Hero.css";
 import "../../css/Buttons.css";
-import SearchBar from "../searchbar/SearchBar";
+import { useNavigate } from 'react-router-dom';
 
 export default function Hero() {
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    navigate('/login');
+  };
+
+  const handleRegister = () => {
+    navigate('/register');
+  };
+
+
+
   return (
     <div>
       <div className="hero-bg">
@@ -11,16 +23,14 @@ export default function Hero() {
           <div>
             <h1>Book your dream stay</h1>
             <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid
-              error repudiandae tenetur accusantium.
+              Find the perfect destination and make unforgettable memories with us.
             </p>
           </div>
           <div className="hero-btns">
-            <button className="btn btn-red">Button</button>
-            <button className="btn btn-red-reverse">Button</button>
+            <button className="btn btn-red" onClick={handleRegister}>Register</button>
+            <button className="btn btn-red-reverse" onClick={handleLogin}>Login</button>
           </div>
         </div>
-        {/* <SearchBar /> */}
       </div>
     </div>
   );
