67ae93244f675ee98841f908dc427a2bf3805b8e
al-sw <alanjunior.sweeney@gmail.com>
2024-07-03 23:23:15 +0200

    Added responsiveness at max-width: 480px

--- hotel-booking-frontend/src/main.jsx
+++ hotel-booking-frontend/src/main.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import App from './App.jsx'
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import App from "./App.jsx";
+import "./index.css";
+import "./css/Phones.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
